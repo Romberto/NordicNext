49cde5dfc5d394de56370977c4babb74ca3bf5a6
Romberto <korhoff@mail.ru>
2026-02-02 19:07:45 +0400

    gt

--- app/layout.tsx
+++ app/layout.tsx
@@ -60,6 +60,19 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <!-- Yandex.Metrika counter -->
+      <script type="text/javascript">
+        (function(m,e,t,r,i,k,a){
+          m[i] = m[i] || function () { (m[i].a = m[i].a || []).push(arguments) };
+        m[i].l=1*new Date();
+        for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+        k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+    })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=106587428', 'ym');
+
+        ym(106587428, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", referrer: document.referrer, url: location.href, accurateTrackBounce:true, trackLinks:true});
+      </script>
+      <noscript><div><img src="https://mc.yandex.ru/watch/106587428" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+      <!-- /Yandex.Metrika counter -->
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
