b4db48492279a999e946dfec7562b9364ef7223a
Romberto <korhoff@mail.ru>
2026-02-02 19:22:11 +0400

    yandex metrica

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import Footer from "@/components/Footer/Footer";
 import Header from "@/components/Header/Header";
 import CookieBanner from "@/components/CookieBanner";
+import { YandexMetrika } from "@/components/Yandexmetrica";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -60,22 +61,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <!-- Yandex.Metrika counter -->
-      <script type="text/javascript">
-        (function(m,e,t,r,i,k,a){
-          m[i] = m[i] || function () { (m[i].a = m[i].a || []).push(arguments) };
-        m[i].l=1*new Date();
-        for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-        k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
-    })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=106587428', 'ym');
-
-        ym(106587428, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", referrer: document.referrer, url: location.href, accurateTrackBounce:true, trackLinks:true});
-      </script>
-      <noscript><div><img src="https://mc.yandex.ru/watch/106587428" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
-      <!-- /Yandex.Metrika counter -->
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      ><YandexMetrika counterId={106587428} />
         <Header navItems={navItems} />
         {children}
         <Footer />
