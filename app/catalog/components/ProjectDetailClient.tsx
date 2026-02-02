b69a83d3970aed0fe90e9e528d8e978b5f2bba18
unknown <korhoff@mail.ru>
2026-02-02 12:44:56 +0400

    test

--- app/catalog/components/ProjectDetailClient.tsx
+++ app/catalog/components/ProjectDetailClient.tsx
@@ -118,7 +118,7 @@ export default function ProjectDetailClient() {
         </div>
       </div>
 
-      <section className="!py-12">
+      <section className="py-12!">
         <div className="grid grid-cols-1 lg:grid-cols-3 gap-12">
           {/* Main */}
           <div className="lg:col-span-2 space-y-12">
