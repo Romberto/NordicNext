15ec489f954507d02e5a411cdef383a20002a064
Romberto <korhoff@mail.ru>
2026-02-02 21:00:34 +0400

    deploy6

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,16 +1,5 @@
-import Link from "next/link";
+
 
 export default function NotFoundPage() {
-  return (
-    <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
-      <h1 className="text-5xl font-bold mb-4 text-red-600">404</h1>
-      <p className="text-xl mb-2">Страница не найдена</p>
-      <Link
-        href="/"
-        className="mt-4 px-6 py-3 bg-blue-600 text-white rounded hover:bg-blue-700 transition"
-      >
-        На главную
-      </Link>
-    </div>
-  );
+  return null
 }
